7dc9fdf194886e68bafbb3b438eb179d01285dd7
Srikanth <code.srikanthjanjirala@gmail.com>
2023-08-15 20:14:35 +0530

    test

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -8,14 +8,49 @@
                 <div class="card-body">
                     <h1> Register </h1>
 
-                    @if(session::has('error'))
+                    @if(Session::has('error'))
                         <p class="text-danger">{{Session::get('error')}} </p>
                     @endif
-                    <form action="{{ route('register') }}</p>
-                    @endif
+                    <form action="{{ route('register') }}" method="post">
+                    @csrf
+                    @method('post')
+                    <div class="form-group">
+                        <label>Name</label>
+                        <input type="text" name="name" class="form-control" placeholder="Name"/>
+                        @if($errors->has('name'))
+                            <p class="text-danger">{{ $erroes->first('name')}}</p>
+                        @endif
+                    </div>
+                    <div class="form-group">
+                        <label>Email </label>
+                        <input type="email" name="email" class="form-control" placeholder="Email"/>
+                        @if ($errors->has('email'))
+                            <p class="text-danger">{{ $errors->first('email')}}</p>
+                        @endif
+                    </div>
+                    <div class="form-group">
+                        <label>Password</label>
+                        <input type="password" name="password" class="form-control" placeholder="Password"/>
+                        @if ($errors->has('password'))
+                            <p class="text-danger">{{ $errors->first('password') }}</p>
+                        @endif
+                    </div>
+                        <div class="form-group">
+                            <label>Confirm Password </label>
+                            <input type="text" name="password_confirmation" class="form-control" placeholder="Password Confirmation"/>
+                        </div>
+                        <div class="row">
+                            <div class="col-8 text-left">
+                                <a href="#" class="btn btn-link"> Forget Password </a>
+                            </div>
+                            <div class="col-4 text-right">
+                                <input type="submit" class="btn btn-primary" value="Login"/>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
 </div>
-@endsection
+@endsection 
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,12 +1,13 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <title>Bootstrap Example</title>
+  <title>Login & Register</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"></script>
   
   <link rel="stylesheet" type="text/css" href="{{ asset('css/app.css') }}">
 </head>
